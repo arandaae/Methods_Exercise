89d79b93c298dffbeebacb97cc4c57c0abb620ac
Armando Aranda <arandaae19@gmail.com>
2022-04-22 15:16:47 -0500

    Added MethodsExercise-Void folder, changed return type to void

---
+++ MethodsExercise-Void/MethodsExercise-0/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace MethodsExercise_0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Math Operators");
+            Add();
+            Console.WriteLine();
+            Subtract();
+            Console.WriteLine();
+            Multiply();
+            Console.WriteLine();
+            Divide();
+
+        }
+        
+
+        public static void Add()
+        {
+            Console.WriteLine("Add two numbers");
+            Console.WriteLine("Enter your first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int result = num1 + num2;
+            Console.WriteLine($"Your answer is {result}.");
+        }
+
+        public static void Subtract()
+        {
+            Console.WriteLine("Subtract two numbers");
+            Console.WriteLine("Enter your first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int result = num1 - num2;
+            Console.WriteLine($"Your answer is {result}.");
+        }
+
+        public static void Multiply()
+        {
+            Console.WriteLine("Multiply two numbers");
+            Console.WriteLine("Enter your first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int result = num1 * num2;
+            Console.WriteLine($"Your answer is {result}.");
+        }
+
+        public static void Divide()
+        {
+            Console.WriteLine("Divide two numbers");
+            Console.WriteLine("Enter your first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int result = num1 / num2;
+            Console.WriteLine($"Your answer is {result}.");
+        }
+    }
+}
